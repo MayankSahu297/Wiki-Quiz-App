06d2860f8518a9d68c79120548da5c90d828c2bc
MayankSahu297 <mayankbgt297@gmail.com>
2025-12-05 20:47:10 +0530

    front end

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://wiki-quiz-app.onrender.com";
 
 const generateTab = document.getElementById("generateTab");
 const historyTab = document.getElementById("historyTab");
